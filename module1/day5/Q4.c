0a36832d0bfabad7b713424d25b4812729c46d68
vamshi1624 <91879026+vamshi1624@users.noreply.github.com>
2023-06-22 08:30:17 +0000

    commit1 : adding Q4.c : main

---
+++ module1/day5/Q4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+struct Student {
+    int rollno;
+    char name[20];
+    float marks;
+};
+
+void sortStudentsDescending(struct Student* students, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (students[j].marks < students[j + 1].marks) {
+                // Swap the positions of two elements
+                struct Student temp = students[j];
+                students[j] = students[j + 1];
+                students[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int size;
+    printf("Enter the number of students: ");
+    scanf("%d", &size);
+
+    struct Student students[size];
+
+    // Assume the array of structures has been initialized or populated with data
+
+    sortStudentsDescending(students, size);
+
+    // Display the sorted array of structures
+    printf("Sorted students (descending order based on marks):\n");
+    for (int i = 0; i < size; i++) {
+        printf("Student %d:\n", i + 1);
+        printf("Roll No: %d\n", students[i].rollno);
+        printf("Name: %s\n", students[i].name);
+        printf("Marks: %.2f\n", students[i].marks);
+        printf("--------------------\n");
+    }
+
+    return 0;
+}
