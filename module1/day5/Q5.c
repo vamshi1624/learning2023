94375c58ddeb303361291e184d650546e5ce8bb7
vamshi1624 <91879026+vamshi1624@users.noreply.github.com>
2023-06-22 08:31:39 +0000

    commit1 : adding Q5.c : main

---
+++ module1/day5/Q5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Student {
+    int rollno;
+    char name[20];
+    float marks;
+};
+
+void searchStudentByName(const struct Student* students, int size, const char* name) {
+    int found = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (strcmp(students[i].name, name) == 0) {
+            printf("Student found!\n");
+            printf("Roll No: %d\n", students[i].rollno);
+            printf("Name: %s\n", students[i].name);
+            printf("Marks: %.2f\n", students[i].marks);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) {
+        printf("Student not found.\n");
+    }
+}
+
+int main() {
+    int size;
+    printf("Enter the number of students: ");
+    scanf("%d", &size);
+
+    struct Student students[size];
+
+    // Assume the array of structures has been initialized or populated with data
+
+    // Example search: Searching for a student named "John"
+    char searchName[20];
+    printf("Enter the name of the student to search: ");
+    scanf("%s", searchName);
+
+    searchStudentByName(students, size, searchName);
+
+    return 0;
+}
