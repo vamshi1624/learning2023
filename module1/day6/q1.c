4bcd715e936da09db7a4cfb7d7b05d5eb6d717dc
vamshi1624 <91879026+vamshi1624@users.noreply.github.com>
2023-06-22 08:36:29 +0000

    commit1 : adding q1.c : main

---
+++ module1/day6/q1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    FILE *sourceFile, *destinationFile;
+    char ch;
+
+    sourceFile = fopen("source.txt", "r");
+    if (sourceFile == NULL) {
+        printf("Unable to open the source file.\n");
+        return 1;
+    }
+
+    destinationFile = fopen("destination.txt", "w");
+    if (destinationFile == NULL) {
+        printf("Unable to create the destination file.\n");
+        fclose(sourceFile);
+        return 1;
+    }
+
+    while ((ch = fgetc(sourceFile)) != EOF) {
+        fputc(ch, destinationFile);
+    }
+
+    printf("File copied successfully.\n");
+
+    fclose(sourceFile);
+    fclose(destinationFile);
+
+    return 0;
+}
